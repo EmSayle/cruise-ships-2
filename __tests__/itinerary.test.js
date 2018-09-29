01f55f1c5b49b655f23d47172059af4c00d901c9
Emily Sayle <emsayle@gmail.com>
2018-09-29 12:15:32 +0100

    added jest mocks to port and itnierary tests

--- __tests__/itinerary.test.js
+++ __tests__/itinerary.test.js
@@ -1,19 +1,18 @@
 const Itinerary = require('../src/itinerary');
-const Port = require('../src/port');
+
 
 describe('Itinerary', () => {
   it('creates a new instance of Itinerary', () => {
-    const itinerary = new Itinerary();
     expect(new Itinerary()).toBeInstanceOf(Object);
   });
-  // instanciates 2 new port objects - 'lisbon' & 'seville'
-  // then a new itinerary object and passes in the new port objects as an array argument
-  it('checks has ports', () => {
-    const lisbon = new Port('Lisbon');
-    const seville = new Port('Seville');
 
-    const itinerary = new Itinerary([lisbon, seville]);
-    // we expect a new array that holds the new port objects
-    expect(itinerary.ports).toEqual([lisbon, seville]);
+
+  it('checks has ports', () => {
+    const mockLisbon = jest.fn(); // created mock port objects
+    const mockSeville = jest.fn();
+
+    const itinerary = new Itinerary([mockLisbon, mockSeville]);
+
+    expect(itinerary.ports).toEqual([mockLisbon, mockSeville]);
   });
 });
--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,15 +1,15 @@
 const Port = require('../src/port');
-// const Ship = require('../src/ship');
+
 
 describe('Port', () => {
   describe('with ships and itinerary', () => {
     let lisbon;
-    let titanic;
-    let maryRose;
+    let mockTitanic;
+    let mockMaryRose;
     beforeEach(() => {
       lisbon = new Port('Lisbon');
-      titanic = {};
-      maryRose = {};
+      mockTitanic = jest.fn();
+      mockMaryRose = jest.fn();
     });
     it('creates an instance of port', () => {
       expect(lisbon).toBeInstanceOf(Object);
@@ -18,15 +18,15 @@ describe('Port', () => {
       expect(lisbon.name).toEqual('Lisbon');
     });
     it('can add a ship', () => {
-      lisbon.addShip(maryRose);
-      expect(lisbon.ships).toContain(maryRose);
+      lisbon.addShip(mockMaryRose);
+      expect(lisbon.ships).toContain(mockMaryRose);
     });
     it('can remove a ship', () => {
-      lisbon.addShip(titanic);
-      lisbon.addShip(maryRose);
-      lisbon.removeShip(maryRose);
+      lisbon.addShip(mockTitanic);
+      lisbon.addShip(mockMaryRose);
+      lisbon.removeShip(mockMaryRose);
 
-      expect(lisbon.ships).toEqual([titanic]);
+      expect(lisbon.ships).toEqual([mockTitanic]);
     });
   });
 });
